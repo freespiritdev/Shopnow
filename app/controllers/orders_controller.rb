41e72f99f9960d08c3aef3b94a624c93a4ae03fd
Cheri <avyishi@gmail.com>
2015-02-13 09:35:50 -0800

    Updated orders and line items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,22 @@ class OrdersController < ApplicationController
       redirect_to @store, notice: "Your cart is empty"
       return
     end
-
+  
     @order = Order.new
+  end
 
+
+
+  def create
+    @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
+      if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        redirect_to @order, notice: 'Thank you for your order'
+      else
+        render action: 'new'
+      end
+    end
+    
 end
