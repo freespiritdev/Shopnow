cee923906df5ce8f9abd5507bee41c7d0eb78318
Cheri <avyishi@gmail.com>
2015-03-08 00:29:16 -0800

    Updated user model and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   get 'orders/index'
 
   resources :products
   resources :carts
   resources :line_items
   resources :orders
-  resources :users
+  
  
 
   get 'stores/index'
